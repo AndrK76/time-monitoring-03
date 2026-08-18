9e1a97cb255016e64c48d578f40f03f0a655cc9e
AndrK76 <andrk-zlt@yandex.ru>
2026-08-19 02:02:44 +0500

    fix 2026-08-18_5

--- front/projects/app-admin/src/app/dialogs/user-profile-dialog/user-profile-dialog.component.ts
+++ front/projects/app-admin/src/app/dialogs/user-profile-dialog/user-profile-dialog.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { ViewCurrentProfileComponent } from '@mon3/sc';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-user-profile-dialog',
@@ -20,8 +21,19 @@ import { ViewCurrentProfileComponent } from '@mon3/sc';
   templateUrl: './user-profile-dialog.component.html',
   styleUrl: './user-profile-dialog.component.scss'
 })
-export class UserProfileDialogComponent {
-  constructor(public dialogRef: MatDialogRef<UserProfileDialogComponent>) {}
+export class UserProfileDialogComponent implements OnInit {
+
+  public dialogRef = inject(MatDialogRef<UserProfileDialogComponent>);
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isXSmall = signal(false);
+
+  ngOnInit(): void {
+    this.breakpointObserver.observe([Breakpoints.XSmall])
+      .subscribe(result => {
+        this.isXSmall.set(result.matches);
+      });
+  }
 
   close(): void {
     this.dialogRef.close();
